9e83480aff03d5c74a10d7b8584c7dd8a206cc6d
UniversXT <universxt@gmail.com>
2022-10-13 10:11:32 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ if [[ $1 == compile ]]; then
         classdate=0
       fi
       if [[ ! -f ${afile%.java}.class || $3 == force || sourcedate -ge classdate ]]; then
-        javac -classpath .:target/dependency/* -d . $afile
+        javac -classpath .:target/dependency/* -d . $afile || exit 1
       fi
     done
   elif [[ -f $2 && ${2##*.} == java ]]; then
@@ -23,12 +23,12 @@ if [[ $1 == compile ]]; then
       classdate=0
     fi
     if [[ ! -f ${2%.java}.class || $3 == force || sourcedate -ge classdate ]]; then
-      javac -classpath .:target/dependency/* -d . $2
+      javac -classpath .:target/dependency/* -d . $2 || exit 1
     fi
   fi
 elif [[ $1 == run ]]; then
   if [[ -f $2 && ${2##*.} == class ]]; then 
-    java -classpath .:target/dependency/* ${2%.class}
+    java -classpath .:target/dependency/* ${2%.class} || exit 1
   elif [[ -f $2 && ${2##*.} == java ]]; then
     sourcedate=$(date -r $2 +%s)
     if [[ -f ${2%.java}.class ]]; then
@@ -37,10 +37,10 @@ elif [[ $1 == run ]]; then
       classdate=0
     fi
     if [[ ! -f ${2%.java}.class || $3 == force || sourcedate -ge classdate ]]; then
-      javac -classpath .:target/dependency/* -d . $2
-      java -classpath .:target/dependency/* ${2%.java}
+      javac -classpath .:target/dependency/* -d . $2 || exit 1
+      java -classpath .:target/dependency/* ${2%.java} || exit 1
     else
-      java -classpath .:target/dependency/* ${2%.java}
+      java -classpath .:target/dependency/* ${2%.java} || exit 1
     fi
   fi
 fi
