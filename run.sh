d5f90362aff201828e3f93f8896c355ecefd7b2d
UniversXT <universxt@gmail.com>
2022-10-17 08:20:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 clear
 cd /home/runner/*/
 
-if [[ $1 == compile ]]; then
-  if [[ $2 == all ]]; then
+function compile() {
+  if [[ $1 == all ]]; then
     for afile in *.java; do
       if [[ ! -f $afile ]]; then break; fi
       sourcedate=$(date -r $afile +%s)
@@ -11,38 +11,37 @@ if [[ $1 == compile ]]; then
       else
         classdate=0
       fi
-      if [[ ! -f ${afile%.java}.class || $3 == force || sourcedate -ge classdate ]]; then
+      if [[ ! -f ${afile%.java}.class || $2 == force || sourcedate -ge classdate ]]; then
         javac -classpath .:target/dependency/* -d . $afile || exit 1
       fi
     done
-  elif [[ -f $2 && ${2##*.} == java ]]; then
-    sourcedate=$(date -r $2 +%s)
-    if [[ -f ${2%.java}.class ]]; then
-      classdate=$(date -r ${2%.java}.class +%s)
+  elif [[ -f $1 && ${1##*.} == java ]]; then
+    sourcedate=$(date -r $1 +%s)
+    if [[ -f ${1%.java}.class ]]; then
+      classdate=$(date -r ${1%.java}.class +%s)
     else
       classdate=0
     fi
-    if [[ ! -f ${2%.java}.class || $3 == force || sourcedate -ge classdate ]]; then
-      javac -classpath .:target/dependency/* -d . $2 || exit 1
+    if [[ ! -f ${1%.java}.class || $2 == force || sourcedate -ge classdate ]]; then
+      javac -classpath .:target/dependency/* -d . $1 || exit 1
     fi
   fi
+}
+
+function run() {
+  if [[ -f $1 && ${1##*.} == class ]]; then 
+    java -classpath .:target/dependency/* ${1%.class} || exit 1
+  elif [[ -f $1 && ${1##*.} == java ]]; then
+    compile $1 $2 || exit 1
+    java -classpath .:target/dependency/* ${1%.java} || exit 1
+  fi
+}
+
+
+if [[ $1 == compile ]]; then
+  compile $2 $3 || exit 1
 elif [[ $1 == run ]]; then
-  if [[ -f $2 && ${2##*.} == class ]]; then 
-    java -classpath .:target/dependency/* ${2%.class} || exit 1
-  elif [[ -f $2 && ${2##*.} == java ]]; then
-    sourcedate=$(date -r $2 +%s)
-    if [[ -f ${2%.java}.class ]]; then
-      classdate=$(date -r ${2%.java}.class +%s)
-    else
-      classdate=0
-    fi
-    if [[ ! -f ${2%.java}.class || $3 == force || sourcedate -ge classdate ]]; then
-      javac -classpath .:target/dependency/* -d . $2 || exit 1
-      java -classpath .:target/dependency/* ${2%.java} || exit 1
-    else
-      java -classpath .:target/dependency/* ${2%.java} || exit 1
-    fi
-  fi
+  run $2 $3 || exit 1
 fi
 
 exit 0
